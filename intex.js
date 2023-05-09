c8b11b0921b1cd5c6706e81c5bd695f84d0d1d6d
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-05-08 21:22:38 -0300

    objetos

---
+++ intex.js
@@ -0,0 +1,66 @@
+
+//  const usuario ={
+// nome:"Maria",
+// idade:20,
+// endereco:["Rua do bobo, numero 0",
+//     "Rua qualquer, numero 120"
+// ]
+
+//  };
+
+//  console.log(usuario)
+
+// usuario.nome = "Roberta"
+
+
+//  console.log(usuario)
+//  console.log(usuario.nome)
+
+/* exércicio 1 */
+
+// const Filme = {
+//     nomedofilme: "Ponte para terabitia",
+//     direcao: "gapor csupó",
+//     lancamento: 2007,
+//     elenco: ["Josh", "Anna",],
+//     assistido: true
+// }
+
+// console.log(Filme.direcao)     
+// console.log(Filme.nomedofilme)
+// console.log(Filme["lancamento"])
+// console.log(Filme["elenco"])
+// console.log(Filme["assistido"]);
+
+
+
+// const Pessoa = {
+//   nome:"Roberta",
+//   idade: 21,
+//   generoMusical: "Rock", 
+
+
+
+// }
+
+// console.log(`o nome da pessoa é ${Pessoa.nome}, ela tem ${Pessoa.idade} anos e gosta muito de ${Pessoa.generoMusical} `)
+
+
+
+// const petPessoa = {
+//     nomedopet: "bucky",
+//     raca: "Pug",
+//     idade: "2",
+
+
+
+// }
+
+
+// const DonoDoPet = {
+//     Nome: "Daniel",
+//     pet: petPessoa
+
+// }
+
+// console.log(DonoDoPet)
