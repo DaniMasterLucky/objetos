db4bde92ec431f5f3323a24a6ea8cc8b8c27ae01
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-05-09 21:30:31 -0300

    atividades

--- intex.js
+++ intex.js
@@ -64,3 +64,89 @@
 // }
 
 // console.log(DonoDoPet)
+
+// const endereco1 = {
+// rua:"rua qualquer", 
+// numero: 20, 
+// cidade: "São paulo",
+// estados: "são paulo",
+// }
+
+// const endereco = {
+//     rua: "brasil",
+//     numero: "4949",
+//     cidade:"são leopoldo",
+//     estados: "rio grande do sul",
+//     }
+
+// endereco.cep = "13.321.432"
+
+
+
+// endereco1.cep = "73.654.425"
+// const pessoa = {
+// nome: "Gabriel",
+// idade:25,
+// generoMusical: "Rock",
+// EnderecoPessoa: [endereco,endereco1]
+
+
+// }
+
+
+// console.log(pessoa)
+
+
+
+//atividade 1 interpretação de código
+
+// const filme = {
+// 	nome: "Auto da Compadecida", 
+// 	ano: 2000, 
+// 	elenco: [
+// 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+// 		"Virginia Cavendish"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+
+ 
+// console.log(filme.elenco[0])             // vai procurar o primeiro número no "elenco"
+// console.log(filme.elenco[filme.elenco.length - 1])        //vai procurar o ultimo numero disponível, por exemplo, Denise fraga.  
+// console.log(filme.transmissoesHoje[2])    // Vai imprimir toda a última linha. 
+
+
+//atividade 2 de interpretação de texto
+
+// const cachorro = {
+// 	nome: "Juca", 
+// 	idade: 3, 
+// 	raca: "SRD"
+// }
+
+// const gato = {...cachorro, nome: "Juba"}
+
+// const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+// console.log(cachorro)
+// console.log(gato)
+// console.log(tartaruga)
+
+//vai ser imprimido juca juba e jubo.
+// ela copia os arrays e objetos para fazer alterações.
+
+
+//atividade 3 interpretação de código
+
+
+
+
+
